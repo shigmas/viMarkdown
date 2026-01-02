aa28c98d8fe6e214e0709d16cb58385a8d625e56
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-01-03 03:34:37 +0900

    0261 ◎ C 01/03 01/03 01002│ToDo│１行目に <!-- --> コメントがある場合、２行目のタイトルを保存時デフォルトファイル名に

--- viMarkdown/viMarkdown/DocWidget.cpp
+++ viMarkdown/viMarkdown/DocWidget.cpp
@@ -17,9 +17,27 @@ void DocWidget::setModified(bool b) {
 }
 QString DocWidget::getTitle() const {
 	QString title;
-	auto hln = m_markdownViewer->getHeadingsLineNum();
-	if( !hln.empty() && hln[0] == 0 ) {		//	1行目が見出し行
+#if 0
+	auto hbn = m_markdownViewer->getHeadingsBlockNum();
+	if( !hbn.empty() && hbn[0] == 0 ) {		//	（コメントを無視した）最初の行が見出し行
 		title = m_markdownViewer->document()->begin().text();
 	}
+#else
+	int ln = 0;
+	QTextDocument *doc = m_mdEditor->document();
+	QTextBlock block = doc->begin();
+	while( block.isValid() && (block.text().startsWith("<!--") || block.text().isEmpty()) ) {
+		++ln;
+		block = block.next();
+	}
+	auto hln = m_markdownViewer->getHeadingsLineNum();
+	if( !hln.empty() && hln[0] == ln ) {		//	コメントを除いた最初の行が見出し行
+		title = block.text();
+		int i = 0;
+		while( i < title.size() && title[i] == u'#' ) ++i;
+		while( i < title.size() && title[i] == u' ' ) ++i;
+		title = title.mid(i);
+	}
+#endif
 	return title;
 }
