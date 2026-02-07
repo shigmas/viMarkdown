c98ec383a7e9ce740e0251eb06e7a451da8c4a45
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-02-07 12:12:54 +0900

    0623 ◎ C 02/07 02/07 01028│ToDo│置換ダイアログ：初期化時に検索履歴文字列を検索CBに設定

--- viMarkdown/viMarkdown/ReplaceDialog.cpp
+++ viMarkdown/viMarkdown/ReplaceDialog.cpp
@@ -1,3 +1,4 @@
+#include <QSettings>
 #include "ReplaceDialog.h"
 
 ReplaceDialog::ReplaceDialog(QWidget *parent)
@@ -5,6 +6,10 @@ ReplaceDialog::ReplaceDialog(QWidget *parent)
 	, ui(new Ui::ReplaceDialogClass())
 {
 	ui->setupUi(this);
+	QSettings settings;
+	QStringList history = settings.value("search/history").toStringList();
+    ui->searchCB->clear();
+	ui->searchCB->addItems(history);
 }
 
 ReplaceDialog::~ReplaceDialog()
