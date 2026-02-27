41be9154e2672099abb41c25e35c0832df2c71c9
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-02-28 04:29:03 +0900

    0848 ◎ C 02/28 02/28 01119│ToDo│エディター > プレースホルダ：画像リンク書式追加 0849 ◎ C 02/28 02/28 01119│ToDo│エディター > プレースホルダ：チェックボックス書式追加

--- viMarkdown/viMarkdown/MainWindow.cpp
+++ viMarkdown/viMarkdown/MainWindow.cpp
@@ -634,9 +634,10 @@ DocWidget *MainWindow::newTabWidget(const QString& title, const QString& fullPat
 	mdEditor->setPlaceholderText("\nここにMarkdownを入力\n"
 									"\nマークダウン書式：\n# タイトル\n## 大見出し\n### 中見出し\n"
 									"本文...（空行：段落区切り）\n行末に半角スペース2つ：強制改行\n"
-									"- リスト\n1. 連番\n"
-									"link: [テキスト](url) or [テキスト](filename.md#見出し)\n"
-									"|見出し|見出し|\n|-----:|------|\n|   123|abc   |\n\n"
+									"1. 連番\n- リスト\n- [ ] チェックボックス\n"
+									"リンク: [テキスト](url) or [テキスト](filename.md#見出し)\n"
+									"画像: ![テキスト](image.png) or 画像ペースト\n"
+									"|見出し|見出し|\n|---|---|\n|123|abc|\n\n"
 									"```CSV\nCSV data...\n```\n");
 	MarkdownPreview *markdownPreview = docWidget->m_preview = new MarkdownPreview(this, docWidget, splitter);
 	//##markdownPreview->setReadOnly(true); // プレビューなので読み取り専用に
