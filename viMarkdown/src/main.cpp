444fe79bfe8be99fd84c95619aac93c79b494c1e
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-04-26 16:47:41 +0900

    1306 ◎ C 04/26 04/26 02003│ToDo│Lingust 初期化コードを main.cpp にマージ 1307 ◎ C 04/26 04/26 02003│ToDo│viMarkdown/viMarkdownCMake/ 以下は削除

--- viMarkdown/src/main.cpp
+++ viMarkdown/src/main.cpp
@@ -1,6 +1,8 @@
 #include <QDir>
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +12,17 @@ int main(int argc, char *argv[])
     app.setWindowIcon(QIcon(":/MainWindow/images/viMarkdown.png"));
     QCoreApplication::setOrganizationName("VisualSoftLab");
     QCoreApplication::setApplicationName("viMarkdown");
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "viMarkdown_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
+    
     MainWindow window;
     if (app.arguments().count() > 1) {
         QString filePath = app.arguments().at(1);
