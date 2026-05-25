4b1b46bc43106c89a92a3d22cbc07f14faba7a4d
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-05-26 04:32:28 +0900

    0219 ◎ C 05/26 05/26 03013│ToDo│MainWindow::do_viCmd(...) が長すぎるので、とりあえず挿入モード系コマンド処理を do_vi_insert(...) に分離

--- viMarkdown/src/MainWindow.h
+++ viMarkdown/src/MainWindow.h
@@ -138,6 +138,7 @@ struct ViStatus {
     bool	m_linewiseMoved = false;	//	行単位移動した（jkG等）
     bool	m_linewiseYanked = false;	//	行単位にヤンクされた
     char	m_cdy = ' ';			//	{c|d|y}<move> 部分
+    char	m_fFtT = ' ';			//	{fFtT}
     int		m_repeatCount = 0;		//	vi コマンド繰り返し回数
     QString	m_yankBuffer;
 };
@@ -340,6 +341,9 @@ protected:
     bool ASSERT_EQ(const QString &expected, const QString &actual, int ln);
 
     void	do_viCmd(QChar cmd, QTextCursor&);
+    void	do_vi_insert(QChar cmd, QTextCursor&);			//	iIaAoOsS
+    void	do_vi_operator(QChar cmd, QTextCursor&);		//	{c|d|y}<move>
+    void	do_vi_motion(QChar cmd, QTextCursor&);			//	hjklwW 等
 
 private:
     bool	m_processing = false;
--- viMarkdown/src/vicmd.cpp
+++ viMarkdown/src/vicmd.cpp
@@ -47,84 +47,29 @@ void do_cdy(QTextCursor& cursor) {
 		gvi.m_viCmdMode = false;
 	}
 }
-void MainWindow::do_viCmd(QChar cmd, QTextCursor& cursor) {
-	//if( cmd.isEmpty() ) return;
-	DocWidget *docWidget = getCurDocWidget();
-	if( docWidget == nullptr ) return;
-	bool isEditor = cursor.document() == docWidget->m_editor->document();
-	bool completed = true;
-	int rcnt = getRepeatCount();
-	auto moveMode = gvi.m_cdy == ' ' ? QTextCursor::MoveAnchor : QTextCursor::KeepAnchor;
-	QTextDocument *doc = cursor.document();
+bool do_fFtT(QTextCursor& cursor, QChar ch, int rcnt) {
 	QTextBlock block = cursor.block();
-	g.m_pendingCommand += cmd;
+	int EndBlockPos = block.position() + block.text().size();
+	for(int i = 0; i != rcnt; ++i) {
+		if( gvi.m_fFtT == 'f' ) {
+			for(;;) {
+				if( cursor.position() == EndBlockPos ) return false;
+				cursor.movePosition(QTextCursor::Right);
+				if( cursor.document()->characterAt(cursor.position()) == ch ) break;
+			}
+		} else if( gvi.m_fFtT == 'F' ) {
+			for(;;) {
+				if( cursor.position() == block.position() ) return false;
+				cursor.movePosition(QTextCursor::Left);
+				if( cursor.document()->characterAt(cursor.position()) == ch ) break;
+			}
+		}
+	}
+	return true;
+}
+void MainWindow::do_vi_insert(QChar cmd, QTextCursor& cursor) {
+	QTextBlock block = cursor.block();
 	switch( cmd.unicode() ) {
-	case 'G':
-		if( gvi.m_repeatCount == 0 ) {
-			cursor.movePosition(QTextCursor::End);
-		} else {
-			block = doc->findBlockByNumber(gvi.m_repeatCount - 1);
-			cursor.setPosition(block.position());
-			hat(cursor);
-		}
-		break;
-	case 'g':
-		if( gvi.m_cdy == 'g' ) {
-			cursor.movePosition(QTextCursor::Start);
-		} else if( gvi.m_cdy == ' ' ) {
-			gvi.m_cdy = 'g';
-			completed = false;
-		}
-		break;
-	case 'c':
-		if( gvi.m_cdy == 'c' ) {
-			cursor.beginEditBlock();
-			g.m_editBlockOpen = true;
-			cursor.movePosition(QTextCursor::StartOfBlock);
-			cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
-			cursor.deleteChar();
-			gvi.m_viCmdMode = false;
-		} else if( gvi.m_cdy == ' ' ) {
-			gvi.m_cdy = 'c';
-			completed = false;
-		}
-		break;
-	case 'D':
-		cursor.movePosition(QTextCursor::EndOfBlock, QTextCursor::KeepAnchor);
-		if( cursor.hasSelection() ) {
-			gvi.m_yankBuffer = cursor.selectedText();
-			gvi.m_linewiseYanked = false;
-			cursor.deleteChar();
-		}
-		break;
-	case 'd':
-		if( gvi.m_cdy == 'd' ) {	//	dd
-			cursor.movePosition(QTextCursor::StartOfBlock);
-			cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
-			if( cursor.hasSelection() ) {
-				gvi.m_yankBuffer = cursor.selectedText();
-				gvi.m_linewiseYanked = true;
-				cursor.deleteChar();
-			}
-		} else if( gvi.m_cdy == ' ' ) {
-			gvi.m_cdy = 'd';
-			completed = false;
-		}
-		break;
-	case 'y':
-		if( gvi.m_cdy == 'y' ) {	//	yy
-			cursor.movePosition(QTextCursor::StartOfBlock);
-			cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
-			if( cursor.hasSelection() ) {
-				gvi.m_yankBuffer = cursor.selectedText();
-				gvi.m_linewiseYanked = true;
-				cursor.clearSelection();
-			}
-		} else {
-			gvi.m_cdy = 'y';
-			completed = false;
-		}
-		break;
 	case 's':
 		cursor.beginEditBlock();
 		g.m_editBlockOpen = true;
@@ -146,7 +91,7 @@ void MainWindow::do_viCmd(QChar cmd, QTextCursor& cursor) {
 	case 'I':
 		cursor.movePosition(QTextCursor::StartOfBlock);
 		for(;;) {
-			QChar ch = doc->characterAt(cursor.position());
+			QChar ch = cursor.document()->characterAt(cursor.position());
 			if( ch != u' ' && ch != u'\t' ) break;
 			cursor.movePosition(QTextCursor::Right);
 		}
@@ -164,177 +109,274 @@ void MainWindow::do_viCmd(QChar cmd, QTextCursor& cursor) {
 		do_openline(cursor, false);
 		gvi.m_viCmdMode = false;
 		break;
-	case 'x':
-		while( --rcnt >= 0 && cursor.position() < block.position() + block.text().size() )	//	行末でない場合
-			cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
-		if( cursor.hasSelection() ) {
-			gvi.m_yankBuffer = cursor.selectedText();
-			gvi.m_linewiseYanked = false;
-			cursor.deleteChar();
-		}
-		break;
-	case 'X':
-		while( --rcnt >= 0 && cursor.position() > block.position() )	//	行頭でない場合
-			cursor.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
-		if( cursor.hasSelection() ) {
-			gvi.m_yankBuffer = cursor.selectedText();
-			gvi.m_linewiseYanked = false;
-			cursor.deleteChar();
-		}
-		break;
-	case 'p':
-		if( !gvi.m_yankBuffer.isEmpty() ) {
-			if( gvi.m_linewiseYanked ) {
-				cursor.movePosition(QTextCursor::NextBlock);
-				//do_openline(cursor, false);
-			} else
-				cursor.movePosition(QTextCursor::Right);
-			cursor.insertText(gvi.m_yankBuffer);
-		}
-		break;
-	case 'P':
-		if( !gvi.m_yankBuffer.isEmpty() ) {
-			if( gvi.m_linewiseYanked ) {
-				cursor.movePosition(QTextCursor::StartOfBlock);
-				//do_openline(cursor, true);
+	}
+}
+void MainWindow::do_viCmd(QChar cmd, QTextCursor& cursor) {
+	//if( cmd.isEmpty() ) return;
+	DocWidget *docWidget = getCurDocWidget();
+	if( docWidget == nullptr ) return;
+	bool isEditor = cursor.document() == docWidget->m_editor->document();
+	bool completed = true;
+	int rcnt = getRepeatCount();
+	if( gvi.m_fFtT == 'f' || gvi.m_fFtT == 'F' || gvi.m_fFtT == 't' || gvi.m_fFtT == 'T' ) {
+		if( do_fFtT(cursor, cmd, rcnt) )
+			do_cdy(cursor);
+	} else if( cmd == 'i' || cmd == 'I' || cmd == 'a' || cmd == 'A' || cmd == 'o' || cmd == 'O' || cmd == 's' || cmd == 'S' ) {
+		do_vi_insert(cmd, cursor);
+	} else {
+		auto moveMode = gvi.m_cdy == ' ' ? QTextCursor::MoveAnchor : QTextCursor::KeepAnchor;
+		QTextDocument *doc = cursor.document();
+		QTextBlock block = cursor.block();
+		g.m_pendingCommand += cmd;
+		switch( cmd.unicode() ) {
+		case 'f':
+		case 'F':
+		case 't':
+		case 'T':
+			if( gvi.m_fFtT == ' ' ) {
+				gvi.m_fFtT = cmd.unicode();
+				completed = false;
 			}
-			cursor.insertText(gvi.m_yankBuffer);
-		}
-		break;
-	case 'u':
-		if( isEditor )
-			docWidget->m_editor->undo();
-		else
-			docWidget->m_preview->undo();
-		break;
-	case 'U':
-		if( isEditor )
-			docWidget->m_editor->redo();
-		else
-			docWidget->m_preview->redo();
-		break;
-	case 'w':
-		for(int i = 0; i < rcnt; ++i) {
-			auto pos = cursor.position();
+			break;
+		case 'G':
+			if( gvi.m_repeatCount == 0 ) {
+				cursor.movePosition(QTextCursor::End);
+			} else {
+				block = doc->findBlockByNumber(gvi.m_repeatCount - 1);
+				cursor.setPosition(block.position());
+				hat(cursor);
+			}
+			break;
+		case 'g':
+			if( gvi.m_cdy == 'g' ) {
+				cursor.movePosition(QTextCursor::Start);
+			} else if( gvi.m_cdy == ' ' ) {
+				gvi.m_cdy = 'g';
+				completed = false;
+			}
+			break;
+		case 'c':
+			if( gvi.m_cdy == 'c' ) {
+				cursor.beginEditBlock();
+				g.m_editBlockOpen = true;
+				cursor.movePosition(QTextCursor::StartOfBlock);
+				cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
+				cursor.deleteChar();
+				gvi.m_viCmdMode = false;
+			} else if( gvi.m_cdy == ' ' ) {
+				gvi.m_cdy = 'c';
+				completed = false;
+			}
+			break;
+		case 'D':
+			cursor.movePosition(QTextCursor::EndOfBlock, QTextCursor::KeepAnchor);
+			if( cursor.hasSelection() ) {
+				gvi.m_yankBuffer = cursor.selectedText();
+				gvi.m_linewiseYanked = false;
+				cursor.deleteChar();
+			}
+			break;
+		case 'd':
+			if( gvi.m_cdy == 'd' ) {	//	dd
+				cursor.movePosition(QTextCursor::StartOfBlock);
+				cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
+				if( cursor.hasSelection() ) {
+					gvi.m_yankBuffer = cursor.selectedText();
+					gvi.m_linewiseYanked = true;
+					cursor.deleteChar();
+				}
+			} else if( gvi.m_cdy == ' ' ) {
+				gvi.m_cdy = 'd';
+				completed = false;
+			}
+			break;
+		case 'y':
+			if( gvi.m_cdy == 'y' ) {	//	yy
+				cursor.movePosition(QTextCursor::StartOfBlock);
+				cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, rcnt);
+				if( cursor.hasSelection() ) {
+					gvi.m_yankBuffer = cursor.selectedText();
+					gvi.m_linewiseYanked = true;
+					cursor.clearSelection();
+				}
+			} else {
+				gvi.m_cdy = 'y';
+				completed = false;
+			}
+			break;
+		case 'x':
+			while( --rcnt >= 0 && cursor.position() < block.position() + block.text().size() )	//	行末でない場合
+				cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+			if( cursor.hasSelection() ) {
+				gvi.m_yankBuffer = cursor.selectedText();
+				gvi.m_linewiseYanked = false;
+				cursor.deleteChar();
+			}
+			break;
+		case 'X':
+			while( --rcnt >= 0 && cursor.position() > block.position() )	//	行頭でない場合
+				cursor.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
+			if( cursor.hasSelection() ) {
+				gvi.m_yankBuffer = cursor.selectedText();
+				gvi.m_linewiseYanked = false;
+				cursor.deleteChar();
+			}
+			break;
+		case 'p':
+			if( !gvi.m_yankBuffer.isEmpty() ) {
+				if( gvi.m_linewiseYanked ) {
+					cursor.movePosition(QTextCursor::NextBlock);
+					//do_openline(cursor, false);
+				} else
+					cursor.movePosition(QTextCursor::Right);
+				cursor.insertText(gvi.m_yankBuffer);
+			}
+			break;
+		case 'P':
+			if( !gvi.m_yankBuffer.isEmpty() ) {
+				if( gvi.m_linewiseYanked ) {
+					cursor.movePosition(QTextCursor::StartOfBlock);
+					//do_openline(cursor, true);
+				}
+				cursor.insertText(gvi.m_yankBuffer);
+			}
+			break;
+		case 'u':
 			if( isEditor )
-				docWidget->m_editor->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				docWidget->m_editor->undo();
 			else
-				docWidget->m_preview->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
-			if( cursor.position() == pos ) break;
-		}
-		do_cdy(cursor);
-		break;
-	case 'W':
-		//do_Word(cursor, rcnt);
-		for(int i = 0; i < rcnt; ++i) {
-			//if( cursor.position() == doc->characterCount() ) break;
-			while( cursor.position() < doc->characterCount() && !isSpaceChar(doc->characterAt(cursor.position())) )
-				cursor.movePosition(QTextCursor::Right);
-			while( cursor.position() < doc->characterCount() && isSpaceChar(doc->characterAt(cursor.position())) )
-				cursor.movePosition(QTextCursor::Right);
-			if( cursor.position() == doc->characterCount() ) break;
-		}
-		do_cdy(cursor);
-		break;
-#if 0
-		for(int i = 0; i < rcnt; ++i) {
-			auto pos = cursor.position();
+				docWidget->m_preview->undo();
+			break;
+		case 'U':
 			if( isEditor )
-				docWidget->m_editor->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				docWidget->m_editor->redo();
 			else
-				docWidget->m_preview->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
-			if( cursor.position() == pos ) break;
-		}
-#endif
-		do_cdy(cursor);
-		break;
-	case 'b':
-		for(int i = 0; i < rcnt; ++i) {
-			if( isEditor )
-				docWidget->m_editor->moveToPrevWord(cursor, /*select = */gvi.m_cdy != ' ');
-			else
-				docWidget->m_preview->moveToPrevWord(cursor, /*select = */gvi.m_cdy != ' ');
-			if( cursor.position() == 0 ) break;
-		}
-		do_cdy(cursor);
-		break;
-	case 'B':
-		for(int i = 0; i < rcnt; ++i) {
-			while( cursor.position() > 0 && isSpaceChar(doc->characterAt(cursor.position()-1)) )
-				cursor.movePosition(QTextCursor::Left);
-			while( cursor.position() > 0 && !isSpaceChar(doc->characterAt(cursor.position()-1)) )
-				cursor.movePosition(QTextCursor::Left);
-			if( cursor.position() == 0 ) break;
-		}
-		do_cdy(cursor);
-		break;
-		break;
-	case 'k':
-		cursor.movePosition(QTextCursor::Up, moveMode, rcnt);
-		do_cdy(cursor);
-		break;
-	case 'j':
-		cursor.movePosition(QTextCursor::Down, moveMode, rcnt);
-		do_cdy(cursor);
-		break;
-	case 'h': {
-		rcnt = qMin(rcnt, cursor.position() - block.position());	//	行頭対応
-		cursor.movePosition(QTextCursor::Left, moveMode, rcnt);
-		do_cdy(cursor);
-		break;
-	}
-	case 'l':
-	case ' ': {
-		int pos = block.position() + block.text().size() - 1;
-		if( cursor.position() < pos ) {
-			rcnt = qMin(rcnt, pos - cursor.position());
-			cursor.movePosition(QTextCursor::Right, moveMode, rcnt);
-		}
-		do_cdy(cursor);
-		break;
-	}
-	case '$':
-		if( !block.text().isEmpty() ) {
-			cursor.setPosition(block.position() + block.text().size() - 1, moveMode);
-		}
-		do_cdy(cursor);
-		break;
-	case '-':
-		cursor.movePosition(QTextCursor::PreviousBlock, moveMode, rcnt);
-		hat(cursor, moveMode);
-		do_cdy(cursor);
-		break;
-	case '\n':
-	case '+':
-		cursor.movePosition(QTextCursor::NextBlock, moveMode, rcnt);
-		hat(cursor, moveMode);
-		do_cdy(cursor);
-		break;
-	case '^':
-		hat(cursor, moveMode);
-		do_cdy(cursor);
-		break;
-	case '0':
-		if( gvi.m_repeatCount == 0 ) {
-			cursor.movePosition(QTextCursor::StartOfBlock, moveMode);
+				docWidget->m_preview->redo();
+			break;
+		case 'w':
+			for(int i = 0; i < rcnt; ++i) {
+				auto pos = cursor.position();
+				if( isEditor )
+					docWidget->m_editor->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				else
+					docWidget->m_preview->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				if( cursor.position() == pos ) break;
+			}
+			do_cdy(cursor);
+			break;
+		case 'W':
+			//do_Word(cursor, rcnt);
+			for(int i = 0; i < rcnt; ++i) {
+				//if( cursor.position() == doc->characterCount() ) break;
+				while( cursor.position() < doc->characterCount() && !isSpaceChar(doc->characterAt(cursor.position())) )
+					cursor.movePosition(QTextCursor::Right);
+				while( cursor.position() < doc->characterCount() && isSpaceChar(doc->characterAt(cursor.position())) )
+					cursor.movePosition(QTextCursor::Right);
+				if( cursor.position() == doc->characterCount() ) break;
+			}
+			do_cdy(cursor);
+			break;
+	#if 0
+			for(int i = 0; i < rcnt; ++i) {
+				auto pos = cursor.position();
+				if( isEditor )
+					docWidget->m_editor->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				else
+					docWidget->m_preview->moveToNextWord(cursor, /*select = */gvi.m_cdy != ' ');
+				if( cursor.position() == pos ) break;
+			}
+	#endif
+			do_cdy(cursor);
+			break;
+		case 'b':
+			for(int i = 0; i < rcnt; ++i) {
+				if( isEditor )
+					docWidget->m_editor->moveToPrevWord(cursor, /*select = */gvi.m_cdy != ' ');
+				else
+					docWidget->m_preview->moveToPrevWord(cursor, /*select = */gvi.m_cdy != ' ');
+				if( cursor.position() == 0 ) break;
+			}
+			do_cdy(cursor);
+			break;
+		case 'B':
+			for(int i = 0; i < rcnt; ++i) {
+				while( cursor.position() > 0 && isSpaceChar(doc->characterAt(cursor.position()-1)) )
+					cursor.movePosition(QTextCursor::Left);
+				while( cursor.position() > 0 && !isSpaceChar(doc->characterAt(cursor.position()-1)) )
+					cursor.movePosition(QTextCursor::Left);
+				if( cursor.position() == 0 ) break;
+			}
+			do_cdy(cursor);
+			break;
+			break;
+		case 'k':
+			cursor.movePosition(QTextCursor::Up, moveMode, rcnt);
+			do_cdy(cursor);
+			break;
+		case 'j':
+			cursor.movePosition(QTextCursor::Down, moveMode, rcnt);
+			do_cdy(cursor);
+			break;
+		case 'h': {
+			rcnt = qMin(rcnt, cursor.position() - block.position());	//	行頭対応
+			cursor.movePosition(QTextCursor::Left, moveMode, rcnt);
 			do_cdy(cursor);
 			break;
 		}
-		//	するすると下にスルーする
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-		gvi.m_repeatCount = gvi.m_repeatCount * 10 + (cmd.unicode() - u'0');
-		completed = false;
-		break;
-	default:
-		completed = false;
+		case 'l':
+		case ' ': {
+			int pos = block.position() + block.text().size() - 1;
+			if( cursor.position() < pos ) {
+				rcnt = qMin(rcnt, pos - cursor.position());
+				cursor.movePosition(QTextCursor::Right, moveMode, rcnt);
+			}
+			do_cdy(cursor);
+			break;
+		}
+		case '$':
+			if( !block.text().isEmpty() ) {
+				cursor.setPosition(block.position() + block.text().size() - 1, moveMode);
+			}
+			do_cdy(cursor);
+			break;
+		case '-':
+			cursor.movePosition(QTextCursor::PreviousBlock, moveMode, rcnt);
+			hat(cursor, moveMode);
+			do_cdy(cursor);
+			break;
+		case '\n':
+		case '+':
+			cursor.movePosition(QTextCursor::NextBlock, moveMode, rcnt);
+			hat(cursor, moveMode);
+			do_cdy(cursor);
+			break;
+		case '^':
+			hat(cursor, moveMode);
+			do_cdy(cursor);
+			break;
+		case '0':
+			if( gvi.m_repeatCount == 0 ) {
+				cursor.movePosition(QTextCursor::StartOfBlock, moveMode);
+				do_cdy(cursor);
+				break;
+			}
+			//	するすると下にスルーする
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			gvi.m_repeatCount = gvi.m_repeatCount * 10 + (cmd.unicode() - u'0');
+			completed = false;
+			break;
+		default:	//	不正コマンド
+			//	undone: エラー表示
+			completed = true;
+		}
 	}
 	if( completed ) {	//	コマンド完結
 		gvi.m_repeatCount = 0;
